f1a4ea9155301de15c2c61088d0ba039d4f4f8e5
rhc-facanono <rhc.oliveira@unesp.br>
2026-04-03 15:15:39 -0300

    vite config atd

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
